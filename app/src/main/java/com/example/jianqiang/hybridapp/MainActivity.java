e5a73165073468b613a826051ec9f8992194d7d9
Acker <gjyvip@yeah.net>
2017-11-06 19:44:49 +0800

    finish unzipping to sdcard

--- app/src/main/java/com/example/jianqiang/hybridapp/MainActivity.java
+++ app/src/main/java/com/example/jianqiang/hybridapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.jianqiang.hybridapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.jianqiang.hybridapp.tools.DirTraversal;
 import com.example.jianqiang.hybridapp.tools.ZipUtils;
@@ -16,12 +23,24 @@ import com.example.jianqiang.hybridapp.tools.ZipUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.zip.ZipException;
 
 public class MainActivity extends AppCompatActivity {
 
-    String pathString = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private static final String pathString = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final Map<String[], Integer> PERMISSION_MAP = new HashMap<String[], Integer>() {
+        {
+            put(PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //进入到WebView页面
-        Button button1 = (Button)findViewById(R.id.button1);
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,19 +56,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button2 = (Button)findViewById(R.id.button2);
+        Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    ZipUtils.unApkFile(MainActivity.this, "hybrid.zip", "解压到本地路径", true);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (checkOrRequestPermission(PERMISSIONS_STORAGE)) {
+                    unzip2Sd();
                 }
             }
         });
 
-        Button button3 = (Button)findViewById(R.id.button3);
+        Button button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button4 = (Button)findViewById(R.id.button4);
+        Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button5 = (Button)findViewById(R.id.button5);
+        Button button5 = (Button) findViewById(R.id.button5);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,5 +93,47 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // User agree the permission
+                    unzip2Sd();
+                } else {
+                    // User disagree the permission
+                    Toast.makeText(this, "您拒绝授予本APP SD卡使用权限", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+        }
+    }
+
+    private boolean checkOrRequestPermission(String[] permissions) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permissions[0]) != PackageManager.PERMISSION_GRANTED) {
+            // Do not have the permission, request it.
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, PERMISSION_MAP.get(permissions));
+            return false;
+        } else {
+            // Have gotten the permission
+            return true;
+        }
+    }
+
+    private void unzip2Sd() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ZipUtils.unApkFile(getApplication(), "hybrid.zip", pathString+ File.separator + "hybrid", true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 
 }
